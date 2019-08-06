948be4301455aa127c9a10d205a7fbf875c17a84
Roman_Taran <roman.taran@pearson.com>
2019-08-06 16:53:54 +0300

    Added object detection on video streaming

--- src/components/viewer/index.js
+++ src/components/viewer/index.js
@@ -1,24 +1,101 @@
 import React, { useEffect } from 'react'
 import { Button } from '../button'
 import './styles.scss'
+import * as cocoSsd from '@tensorflow-models/coco-ssd'
 
 export const Viewer = () => {
-	const video = React.createRef()
+	const videoRef = React.createRef()
+	const canvasRef = React.createRef()
 
 	useEffect(() => {
-		navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-			.then(stream => {
-				video.current.srcObject = stream
-			})
-			.catch(err => {
-				console.log(err)
-			})
+		const img = document.getElementById('img');
+
+		cocoSsd.load().then(model => {
+			if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+				const webCamPromise = navigator.mediaDevices
+					.getUserMedia({
+						audio: false,
+						video: {
+							facingMode: "user"
+						}
+					})
+					.then(stream => {
+						window.stream = stream;
+						videoRef.current.srcObject = stream;
+						return new Promise((resolve, reject) => {
+							videoRef.current.onloadedmetadata = () => {
+								resolve();
+							};
+						});
+					});
+				const modelPromise = cocoSsd.load();
+				Promise.all([modelPromise, webCamPromise])
+					.then(values => {
+						detectFrame(videoRef.current, values[0]);
+					})
+					.catch(error => {
+						console.error(error);
+					});
+			}
+		});
+
+		const detectFrame = (video, model) => {
+			model.detect(video).then(predictions => {
+				renderPredictions(predictions);
+				requestAnimationFrame(() => {
+					detectFrame(video, model);
+				});
+			});
+		};
+
+		const renderPredictions = predictions => {
+			const ctx = canvasRef.current.getContext("2d");
+			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+			// Font options.
+			const font = "16px sans-serif";
+			ctx.font = font;
+			ctx.textBaseline = "top";
+			predictions.forEach(prediction => {
+				const x = prediction.bbox[0];
+				const y = prediction.bbox[1];
+				const width = prediction.bbox[2];
+				const height = prediction.bbox[3];
+				// Draw the bounding box.
+				ctx.strokeStyle = "#00FFFF";
+				ctx.lineWidth = 4;
+				ctx.strokeRect(x, y, width, height);
+				// Draw the label background.
+				ctx.fillStyle = "#00FFFF";
+				const textWidth = ctx.measureText(prediction.class).width;
+				const textHeight = parseInt(font, 10); // base 10
+				ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+			});
+
+			predictions.forEach(prediction => {
+				const x = prediction.bbox[0];
+				const y = prediction.bbox[1];
+				// Draw the text last to ensure it's on top.
+				ctx.fillStyle = "#000000";
+				ctx.fillText(prediction.class, x, y);
+			});
+		};
+
+
+
+		// navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+		// 	.then(stream => {
+		// 		videoRef.current.srcObject = stream
+		// 	})
+		// 	.catch(err => {
+		// 		console.log(err)
+		// 	})
 	})
 
 	return (
 		<div className="viewer">
 			<div className="viewer__inner">
-				<video ref={video} id="video" autoPlay> </video>
+				<video ref={videoRef} id="video" width="600" height="600" autoPlay> </video>
+				<canvas ref={canvasRef} id="canvas" width="600" height="600"> </canvas>
 			</div>
 			<div className="viewer__footer">
 				<Button text="run"/>
