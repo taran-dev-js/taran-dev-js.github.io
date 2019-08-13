d0c6979f758cf180dd68841985c74dcd7ff733ad
Roman_Taran <roman.taran@pearson.com>
2019-08-13 15:44:36 +0300

    Added model

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import { Viewer } from './components/viewer'
-import { Header } from './components/header'
-
 
 function App () {
 	return (
 		<main className="App">
 			<div className="container-fluid">
-				<Header count={0}/>
 				<Viewer/>
 			</div>
 		</main>
