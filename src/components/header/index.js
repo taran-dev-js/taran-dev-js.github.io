24f960c6c2a9650bdaf8a5c384d3e8be4a02726c
Roman_Taran <roman.taran@pearson.com>
2019-11-01 16:22:28 +0200

    refactor for demo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = ({count}) => {
 	return (
 		<header>
-			<p>find people: <b>{count}</b></p>
+			<p>Count of people: <b>{count}</b></p>
 		</header>
 	)
 }
