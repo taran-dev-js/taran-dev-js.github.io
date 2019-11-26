48071df9b04da493b2846254b2ebb3089d6c692d
Roman_Taran <roman.taran@pearson.com>
2019-11-26 14:15:13 +0200

    add posenet

--- src/components/viewer/index.js
+++ src/components/viewer/index.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import * as cocoSsd from '@tensorflow-models/coco-ssd'
+import * as posenet from '@tensorflow-models/posenet'
 import { Header } from '../header'
 import { Canvas } from '../canvas'
 import { renderPredictions } from './renderPredictions'
+import { handDetection } from './handDetection'
+import { drawKeypoints, drawSkeleton } from './drawUtils'
 import { ReactComponent as SettingIcon } from '../../assets/gear.svg'
 import { ReactComponent as SaveIcon } from '../../assets/floppy-disk.svg'
 import WebCam from 'react-webcam'
@@ -12,26 +15,94 @@ export const Viewer = () => {
 	const canvasRef = React.createRef();
 	const webcamRef = React.useRef(null);
 	const [count, setCount] = useState(0);
-	const [heightCanvas, setHeight] = useState(0);
 	const [loader, setLoader] = useState(false);
+	const videoConfig = {
+		height: 720,
+		width: 420
+	};
+	const posenetConfig = {
+		architecture: 'MobileNetV1',
+		outputStride: 16,
+		// inputResolution: 449,
+		multiplier: 1.0,
+	}
 
-	useEffect(() => {
-		setHeight(canvasRef.current.clientHeight)
+	const startVideo = () => {
 		if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 			const modelPromise = cocoSsd.load();
+			const poseNetLoad = posenet.load(posenetConfig)
 			setLoader(true)
 
-			Promise.all([modelPromise ])
+			Promise.all([modelPromise, poseNetLoad ])
 				.then(values => {
 					setLoader(false)
-					detectFrame(webcamRef.current.video, values[0]);
+					// detectFrame(webcamRef.current.video, values[0]);
+					startPoseNet(webcamRef.current.video, values[1])
 				})
 				.catch(error => {
 					console.error(error);
 				});
-
 		}
+	}
+
+	const canvasVideo = () => {
+		// console.log(webcamRef.current.video.clientWidth);
+	}
+
+	useEffect(() => {
+		startVideo()
 	}, [])
+
+	const startPoseNet = (video, net) => {
+
+		const width = webcamRef.current.video.clientWidth
+		const height = webcamRef.current.video.clientHeight
+		const canvas = canvasRef.current
+		const ctx = canvas.getContext('2d')
+		canvas.width = width
+		canvas.height = height
+
+		async function poseDetectionFrame () {
+			let color = '#30ee08'
+			let poses = []
+			let minPoseConfidence
+			let minPartConfidence
+			let all_poses = await net.estimateMultiplePoses(video, {
+				flipHorizontal: false,
+				decodingMethod: 'multi-person',
+				maxDetections: 3,
+				scoreThreshold: 0.6,
+			})
+
+			poses = poses.concat(all_poses)
+			minPoseConfidence = 0.15
+			minPartConfidence = 0.1
+			// console.log(poses)
+			ctx.clearRect(0, 0, width, height)
+			ctx.save()
+			ctx.scale(-1, 1)
+			ctx.drawImage(video, 0, 0, width, height)
+			ctx.restore()
+			poses.forEach(({ score, keypoints }) => {
+				if (score >= minPoseConfidence) {
+					if (!keypoints.slice(5, 11).every(x => x.score > minPartConfidence)) {
+						return
+					}
+					const handTrigger = handDetection(keypoints, height, width)
+					// if (handTrigger) alert(handTrigger)
+					color = handTrigger ? '#eeee33' : '#477eff';
+					drawSkeleton(keypoints, minPartConfidence, ctx, 1, color)
+					drawKeypoints(keypoints, minPartConfidence, ctx, 1, color)
+				}
+			})
+
+			// detectFrame(webcamRef.current., cocossd, color);
+
+			requestAnimationFrame(poseDetectionFrame)
+		}
+
+		poseDetectionFrame()
+	}
 
 	const detectFrame = (video, model) => {
 		model.detect(video).then(predictions => {
@@ -48,18 +119,16 @@ export const Viewer = () => {
 	};
 
 	const videoConstraints = {
-		width: 720,
-		height: 480,
+		width: videoConfig.height,
+		height: videoConfig.width,
 		audio: false,
+		className: 'video',
 		facingMode: "environment",
 	};
 
-
 	const mapPerson = prediction =>  prediction.filter(item => item.class === 'person')
 
-	const styles = {
-		height: heightCanvas
-	}
+	const styles = { height: videoConfig.height }
 
 	return (
 		<div className="container-fluid">
@@ -67,14 +136,15 @@ export const Viewer = () => {
 			<div className={`viewer ${loader ? 'loader' : ''}`} style={styles}>
 				<WebCam videoConstraints={videoConstraints}
 						audio={false}
-						height="720"
-						width="480"
+						width={videoConfig.width}
+						height={videoConfig.height}
+						onUserMedia={canvasVideo}
 						ref={webcamRef}/>
-				<Canvas canvasRef={canvasRef} width={480} height={720}/>
+				<Canvas canvasRef={canvasRef} width={videoConfig.width} height={videoConfig.height}/>
 			</div>
 			<div className="btn-container">
-				<button className="btn-blue"><SettingIcon width={18} height={18} fill="#fff"/></button>
 				<button className="btn-blue"><SaveIcon width={18} height={18} fill="#fff"/></button>
+				<button className="btn-blue"><SettingIcon width={18} height={18} fill="#fff"/></button>
 			</div>
 		</div>
 	)
